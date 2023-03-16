4ed6639a1ac6e1878de9da439e210b0c33566605
Pavel Simakov <psimakov@users.noreply.github.com>
2023-03-16 13:22:17 -0400

    Update main.tf
    Signed-off-by: Pavel Simakov <psimakov@users.noreply.github.com>

--- deploy/main.tf
+++ deploy/main.tf
@@ -24,6 +24,9 @@ locals {
   ]
 }
 
+# Data source to access GCP project metadata 
+data "google_project" "project" {}
+
 
 # Enable the required GCP APIs
 resource "google_project_service" "default" {
